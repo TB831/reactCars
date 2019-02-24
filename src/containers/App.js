ad8cded142b2dc21796bf69ae197a9972996f223
TB831 <druu831@gmail.com>
2019-02-23 19:17:56 -0800

    added constructor to App Class. Unsplash data now saved into state after request

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,22 +6,33 @@ import './App.css';
 const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    }
+  }
 
   onSearchSubmit = (term) => {
-    axios.get('https://api.unsplash.com/search/photos?', {
+    axios.get('https://api.unsplash.com/search/photos?', {  // GET req to unsplash
       params: {
         query: term
       },
       headers: {
         Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
       }
-    });
+    })
+      .then((res) => this.setState({ images: res.data.results }))
+      .catch((err) => console.log(err));
   }
 
   render() {
+    const { images } = this.state;
+
     return (
       <div className='ui container searchbar'>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Images: {images.length}
       </div>
     );  
   }
