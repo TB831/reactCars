8bc2d241d9aec624a4d801325e2d8be3387f8d10
TB831 <druu831@gmail.com>
2019-02-23 17:56:51 -0800

    Added axios package. Tested GET req to Unsplash API

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from '../components/SearchBar';
 import './App.css';
 
+const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
 class App extends Component {
+
   onSearchSubmit = (term) => {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos?', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
+      }
+    });
   }
 
   render() {
